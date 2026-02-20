bc6ee6876748f9b7b56d881c96042c941818a8eb
Thomas Tay <ai.thomas.tay@outlook.com>
2026-02-20 11:05:15 +0800

    update note and app

--- src/ruff_test.py
+++ src/ruff_test.py
@@ -1,7 +1,60 @@
-def test_func(x, y):
-    result = x + y
-    return result
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+
+# Setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler("./log/app.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
-def bar():
-    print("world")  # Needs two lines between top-level functions
+def add_func(x, y):
+    return x + y
+
+
+def show_data(df):
+    return df.style
+
+
+def main():
+    load_dotenv()
+    passwd = os.getenv("PASSWORD")
+
+    if passwd is None:
+        logger.error("PASSWORD environment variable not set")
+        raise EnvironmentError("PASSWORD environment variable not set")
+    else:
+        logger.info("Password loaded")
+
+    df = pd.DataFrame(np.array(range(1, 10)).reshape(3, 3))
+
+    data = {
+        "Name": ["Martha", "Tim", "Rob", "Georgia"],
+        "Maths": [87, 91, 97, 95],
+        "Science": [83, 99, 84, 76],
+    }
+    df2 = pd.DataFrame(data)
+
+    logger.info("Second dataframe created")
+    logger.info(add_func(2, 3))
+    logger.info(df)
+    logger.info(df2)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
